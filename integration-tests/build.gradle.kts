378ffcc13e9ef1f5a463393e22bb7c35f9708574
Christian Fränkel <fraenkelc@users.noreply.github.com>
2026-04-08 19:18:19 +0200

    fix: declare test dependencies

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -8,6 +8,16 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+val testDependencies by configurations.creating {
+    description = "Test dependencies for use in integration tests"
+    isCanBeResolved = true
+    isCanBeConsumed = false
+}
+
+dependencies {
+    testDependencies(project(":otel-pyroscope-spring-boot-starter", "testDependencies"))
+}
+
 @Suppress("UnstableApiUsage")
 testing {
     suites {
@@ -22,6 +32,25 @@ testing {
                 implementation(libs.json.path)
                 implementation(libs.awaitility)
                 runtimeOnly(libs.logback.classic)
+            }
+            targets.all {
+                testTask {
+                    // the docker-compose build consumes the test dependencies
+                    dependsOn(testDependencies)
+                    // it also uses the following files in the copy action
+                    inputs.files(fileTree(rootProject.projectDir) {
+                        include("gradle.properties")
+                        include("settings.gradle.kts")
+                        include("build.gradle.kts")
+                        include("gradle/libs.versions.toml")
+                        include("otel-pyroscope-spring-boot-starter/build.gradle.kts")
+                        include("otel-pyroscope-spring-boot-starter/src/**")
+                        include("examples/spring-opentelemetry/build.gradle.kts")
+                        include("examples/spring-opentelemetry/settings.gradle.kts")
+                        include("examples/spring-opentelemetry/gradle/libs.versions.toml")
+                        include("examples/spring-opentelemetry/src/**")
+                    })
+                }
             }
         }
     }
